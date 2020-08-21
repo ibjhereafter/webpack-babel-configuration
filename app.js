b14e5df098334df23fdf0046cb23afd8f9aaadc7
Ibrahim Bakar Jalloh <ibjsoftwareengineer@gmail.com>
2020-08-21 19:01:01 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = (
+    <div>
+        <p>Hell from React config file</p>
+    </div>
+);
+
+ReactDOM.render(template, document.getElementById('root'));
